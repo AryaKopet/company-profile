5efada8caeb558ef179edb63145c89663946ebc3
AryaKopet <aryasokker23@gmail.com>
2024-12-29 23:04:16 +0700

    refactor: clean code route.php dan create: model pesanan

---
+++ app/Models/Pesanan.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pesanan extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pesanan'; // Nama tabel
+
+    protected $primaryKey = 'id_pesanan'; // Primary key
+
+    protected $fillable = [
+        'email',
+        'bahan_material',
+        'frame',
+        'panjang',
+        'lebar',
+        'tinggi'
+    ];
+}
--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,12 @@ use App\Http\Controllers\CustomerController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+}); // route awal halaman
+Route::fallback(function(){
+    return view('error');
+}); // route alamat error
 Route::get('/custom-box', [CustomerController::class, 'showStep1'])->name('customize.box.step1');
 Route::post('/customize-box/step1', [CustomerController::class, 'submitStep1'])->name('customize.box.step1.submit');
 Route::get('/custom-box/2', [CustomerController::class, 'showStep2'])->name('customize.box.step2');
 Route::post('/customize-box/step2', [CustomerController::class, 'submitStep2'])->name('customize.storeStep2');
 Route::get('/admin/dashboard', [Dashboard::class, 'render'])->name('filament.pages.dashboard');
-
-Route::fallback(function(){
-    return view('error');
-}); // route alamat error
-
