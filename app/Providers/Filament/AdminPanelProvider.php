d50182412a1d0f7c2c8f522b7d8412cf1987385c
AryaKopet <aryasokker23@gmail.com>
2025-01-09 09:50:07 +0700

    update fitur ganti password dashboard admin

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Pages\Auth\EditProfile;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -9,7 +10,6 @@ use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -23,12 +23,12 @@ class AdminPanelProvider extends PanelProvider
     public function panel(Panel $panel): Panel
     {
         return $panel
-            ->default()
+            ->profile(isSimple: false)
             ->id('admin')
             ->path('admin')
             ->login()
             ->colors([
-                'primary' => color::Blue,
+                '' => color::Blue,
                 // 'danger' => Color::Rose,
                 // 'gray' => Color::Gray,
                 // 'info' => Color::Blue,
--- app/Providers/FilamentServiceProvider.php
+++ app/Providers/FilamentServiceProvider.php
@@ -10,18 +10,13 @@ class FilamentServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        // Mengganti Nama Aplikasi di Filament
         Filament::serving(function () {
-            Filament::registerNavigationGroups([
-                'PT. Sugi Harti Indonesia', // Ganti dengan nama aplikasi yang diinginkan
-            ]);
         });
     }
 
     public function panel(Panel $panel): Panel
     {
         return $panel
-            // Menambahkan favicon
             ->brandLogo(asset('assets/logo.png'));
     }
 }
