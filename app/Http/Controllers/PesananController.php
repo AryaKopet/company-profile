f24f95b6f55a77a21be2fdf7d23f9417800da9b7
AryaKopet <aryasokker23@gmail.com>
2025-01-23 14:37:52 +0700

    update: preview pesanan controller

--- app/Http/Controllers/PesananController.php
+++ app/Http/Controllers/PesananController.php
@@ -45,84 +45,14 @@ class PesananController extends Controller
         $customization['material_name'] = $material->barang ?? 'Material tidak ditemukan';
         $customization['frame_name'] = $frame->barang ?? 'Frame tidak ditemukan';
 
-        // Konstanta
-        $sparePond = 10;
-        $kupingan = 40;
-        $panjangMaterial = 3000;
-        $lebarMaterial = 2000;
-
-        // Fungsi pembulatan khusus
-        $customRound = function ($value) {
-            return ($value - floor($value) >= 0.5) ? ceil($value) : floor($value);
-        };
-
-        // Perhitungan body dan pintu
-        $panjangBody = $customization['tinggi'] + $customization['lebar'] + $customization['tinggi'] + $sparePond;
-        $lebarBody = ($panjangBody - 10) + $sparePond;
-
-        $panjangPintu = $customization['tinggi'] + $kupingan + $sparePond;
-        $lebarPintu = $kupingan + $customization['lebar'] + $kupingan + $sparePond;
-
-        $lembaranBody = ($panjangMaterial / $panjangBody) * ($lebarMaterial / $lebarBody);
-        $hargaBody = $material->harga / $customRound($lembaranBody);
-
-        $lembaranPintu = ($panjangMaterial / $panjangPintu) * ($lebarMaterial / $lebarPintu);
-        $hargaPintu = $material->harga / $customRound($lembaranPintu);
-
-        $hargaBox = $hargaBody + $hargaPintu;
-
-        // Perhitungan frame
-        $totalPanjangFrame = $customization['panjang'] * 2;
-        $totalLebarFrame = $customization['lebar'] * 2;
-        $hargaFrame = ($totalPanjangFrame + $totalLebarFrame) * $frame->harga / $panjangMaterial;
-
-        // Harga komponen tambahan
-        $components = [
-            'corner' => Material::where('barang', 'corner')->value('harga'),
-            'handle' => Material::where('barang', 'handle')->value('harga'),
-            'kanban' => Material::where('barang', 'kanban')->value('harga'),
-            'screw' => Material::where('barang', 'screw')->value('harga'),
-            'mata_itik' => Material::where('barang', 'mata itik')->value('harga'),
-            'printing' => Material::where('barang', 'printing')->value('harga'),
-            'process' => Material::where('barang', 'process')->value('harga'),
-            'die_cut' => Material::where('barang', 'die cut')->value('harga'),
-            'transport' => Material::where('barang', 'transport jabodetabek')->value('harga'),
-        ];
-
-        // Total harga material
-        $hargaTotalMaterial = $hargaBox + $hargaFrame + 
-                              (4 * $components['corner']) + (2 * $components['handle']) +
-                              (2 * $components['kanban']) + (16 * $components['screw']) +
-                              (16 * $components['mata_itik']) + $components['printing'];
-
-        // Harga jasa
-        $hargaJasa = $components['process'] + $components['die_cut'] + $components['transport'];
-        $profit = $hargaTotalMaterial * 0.3;
-        $hargaTotalJasa = $hargaJasa + $profit;
-
-        // Total harga produksi
-        $totalHargaProduksi = $hargaTotalMaterial + $hargaTotalJasa;
-
-        // Simpan pesanan
-        $pesanan = Pesanan::create([
-            'email' => $customer['email'],
-            'bahan_material' => $material->barang,
-            'nama_box' => $customization['nama_box'],
-            'frame' => $frame->barang,
-            'panjang' => $customization['panjang'],
-            'lebar' => $customization['lebar'],
-            'tinggi' => $customization['tinggi'],
-            'harga' => round($totalHargaProduksi, 2),
-        ]);
-
         // Generate nomor struk
-        $nomorStruk = $this->generateNomorStruk($pesanan->id_pesanan);
+        $nomorStruk = $this->generateNomorStruk($request->id_pesanan);
 
         // Data untuk struk
         $strukData = [
             'customer' => $customer,
             'customization' => $customization,
-            'total_harga' => round($totalHargaProduksi, 0),
+            'total_harga' => round($request['total_harga'], 0),
             'nomor_struk' => $nomorStruk,
         ];
 
